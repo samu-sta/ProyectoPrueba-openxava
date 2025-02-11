3f496a63e7d614bd287710d986065e5e3efdc46a
Samuel <samuelfelipelorente@gmail.com>
2025-02-11 10:43:13 +0100

    quitados constructores de LicenciaPorMes

--- src/main/java/com/tuempresa/registrodeaplicaciones/modelo/LicenciaPorMes.java
+++ src/main/java/com/tuempresa/registrodeaplicaciones/modelo/LicenciaPorMes.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 import lombok.*;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class LicenciaPorMes {
 
     private int mes;
@@ -12,18 +12,4 @@ public class LicenciaPorMes {
     private BigDecimal importe;
 
     private long cantidad;
-
-
-    public LicenciaPorMes(int mes, BigDecimal importe, long cantidad) {
-        this.mes = mes;
-        this.importe = importe != null ? importe : BigDecimal.ZERO;
-        this.cantidad = cantidad;
-    }
-
-    // Constructor por defecto requerido por JPA
-    public LicenciaPorMes() {
-        this.importe = BigDecimal.ZERO;
-        this.cantidad = 0;
-    }
-    
 }
